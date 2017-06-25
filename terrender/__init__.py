77735bfba754b99587b80fbfa69543c9e269a49c
Mathias Rav <rav@cs.au.dk>
2017-06-25 11:06:00 -0400

    Add cythonized() decorator

--- terrender/__init__.py
+++ terrender/__init__.py
@@ -52,6 +52,73 @@ class Terrain:
         self.faces = face_points_hom.reshape((n_points // 3, 3, ndim_hom))
 
 
+class DifferentResults(Exception):
+    '''
+    >>> raise DifferentResults('foo', 'a bar', 'an int')
+    Traceback (most recent call last):
+    ...
+    terrender.DifferentResults: foo: Fast returned a bar, slow returned an int
+    '''
+    def __str__(self):
+        return '%s: Fast returned %s, slow returned %s' % self.args
+
+
+def compare_results(path, fast, slow):
+    tfast = type(fast)
+    tslow = type(slow)
+    if tfast != tslow:
+        raise DifferentResults(path, tfast.__name__, tslow.__name__)
+
+    if isinstance(fast, (tuple, list)):
+        if len(fast) != len(slow):
+            raise DifferentResults(
+                path, 'a length-%s %s' % (len(fast), tfast.__name__),
+                'a length-%s %s' % (len(slow), tslow.__name__))
+
+        for i, (x, y) in enumerate(zip(fast, slow)):
+            compare_results('%s[%s]' % (path, i), x, y)
+
+        return
+
+    fast = np.asarray(fast)
+    slow = np.asarray(slow)
+    if fast.shape != slow.shape:
+        raise DifferentResults(
+            path, 'shape %r' % (fast.shape,), 'shape %r' % (slow.shape,))
+    close = np.isclose(fast, slow)
+    incorrect_flat = np.argmin(close.ravel())
+    if close.ravel()[incorrect_flat]:
+        return
+    incorrect_idx = tuple(
+        np.squeeze(np.unravel_index(incorrect_flat, close.shape)))
+    assert not np.isclose(fast[incorrect_idx], slow[incorrect_idx])
+    path += '[%s]' % ', '.join(map(str, incorrect_idx))
+    raise DifferentResults(
+        path, fast[incorrect_idx], slow[incorrect_idx])
+
+
+def cythonized(fn):
+    if __debug__:
+        try:
+            import terrender.test
+            fast_fn = getattr(terrender.test, fn.__name__)
+        except (ImportError, AttributeError):
+            print("Could not import terrender.test.%s" % fn.__name__)
+            return fn
+
+        def wrapper(*args, **kwargs):
+            fast_result = fast_fn(*args, **kwargs)
+            slow_result = fn(*args, **kwargs)
+            compare_results(fn.__name__, fast_result, slow_result)
+            return fast_result
+
+        return wrapper
+
+    import terrender.test
+    return getattr(terrender.test, fn.__name__)
+
+
+@cythonized
 def rot_4d_xy(angle):
     s_angle = np.sin(angle)
     c_angle = np.cos(angle)
