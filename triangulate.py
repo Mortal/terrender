2ae019c8ad0291ab34f12db4b6aa569f467c10ba
Mathias Rav <m@git.strova.dk>
2018-02-22 11:59:29 +0100

    Work on triangulate.py

--- triangulate.py
+++ triangulate.py
@@ -1,5 +1,6 @@
 import functools
 import numpy as np
+import scipy.ndimage
 from terrender.terrain import Terrain
 from terrender.backends.ipe import IpeOutput
 from terrender.projection import project_persp
@@ -8,13 +9,19 @@ from terrender.lighting import flat_shading
 
 
 def main():
-    xs = ys = np.linspace(.5*np.pi, 1.5*np.pi, 7)
-    xx, yy = np.meshgrid(xs, ys)
-    zz = np.sin(xx + 0.4 * yy) / 10
-    t = Terrain.triangulate_grid(zz)
+    im = scipy.ndimage.imread('/home/rav/.openttd/scenario/heightmap/UNNAMED-291-267-70-64.png')
+    assert im.shape == (64, 70), im.shape
+    values = np.unique(im)
+    # im = im[20:40, 20:40]
+    im = np.searchsorted(values, im)
+    im = im.astype(np.float32)
+    t = Terrain.triangulate_grid(im)
 
     altitude = 30
-    field_of_view = 40
+    field_of_view = 0
+    rot = np.radians(45)
+    light_altitude = np.radians(60)
+    light_circumference_angle = np.radians(45/2)
 
     with IpeOutput('grid.ipe') as output:
         altitude_angle = np.radians(altitude - 90)
@@ -25,50 +32,28 @@ def main():
 
         zmin = pmin[2]
         zmax = pmax[2]
-        zscale = 0.5 * focus_radius / (zmax-zmin)
-        if zscale > 1:
-            t.faces[:, :, 2] *= zscale
-            zmin *= zscale
-            zmax *= zscale
-            center[2] *= zscale
+        # zscale = 0.5 * focus_radius / (zmax-zmin)
+        zscale = 0.2
+        t.faces[:, :, 2] *= zscale
+        zmin *= zscale
+        zmax *= zscale
+        center[2] *= zscale
         contour = None  # (zmin + (zmax - zmin) * contour_pos, t.faces[:, :, 2])
 
         project_fun = functools.partial(
             project_persp, focus_center=center[:3], focus_radius=focus_radius,
             field_of_view=np.radians(field_of_view))
 
-        light_circumference_angle = np.radians(210)
-        light = flat_shading(t, light_circumference_angle, np.radians(30))
+        light = flat_shading(t, light_circumference_angle, light_altitude, darkest=0.5)
 
-        if field_of_view:
-            n = 10
-            for i in range(n):
-                with output.open_page() as page:
-                    faces = project_persp(t, center[:3], focus_radius,
-                                          0, 0, np.radians(i*field_of_view/n))
-                    page.faces(z_order(faces), faces, light, contour=contour)
-
-        n = 10
-        for i in range(n):
-            with output.open_page() as page:
-                faces = project_fun(t, circumference_angle=0, altitude_angle=i*altitude_angle/n)
-                page.faces(z_order(faces), faces, light, contour=contour)
-        n = 50
-        for i in range(n):
-            with output.open_page() as page:
-                circ_angle = 2*np.pi*i/n
-                light = flat_shading(t, circ_angle + light_circumference_angle, np.radians(30))
-                faces = project_fun(t, circumference_angle=circ_angle, altitude_angle=altitude_angle)
-                page.faces(z_order(faces), faces, light, contour=contour)
-
-        n = 50
-        faces = project_fun(t, circumference_angle=0, altitude_angle=altitude_angle)
-        z = z_order(faces)
-        for i in range(n):
-            with output.open_page() as page:
-                circ_angle = 2*np.pi*i/n
-                light = flat_shading(t, circ_angle + light_circumference_angle, np.radians(30))
-                page.faces(z, faces, light, contour=contour)
+        bitmap = output.add_bitmap(scipy.ndimage.imread(
+            '/home/rav/.openttd/scenario/heightmap/screenshot-340-303.png'))
+        with output.open_page() as page:
+            page.image(bitmap, 0, 244.359, 595, 597.641,
+                       matrix='1 0 0 1 0 241.847')
+            faces = project_fun(t, circumference_angle=rot, altitude_angle=altitude_angle)
+            with page.group(matrix='3.78083 0 0 3.78083 -1001.62 -1957.4'):
+                page.faces(z_order(faces), faces, light, contour=contour, stroke=None)
 
 
 if __name__ == '__main__':
